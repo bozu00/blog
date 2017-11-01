8e1e530645bec34c51daed2741c6d45276b12640
bozu <test@test.com>
2017-11-02 03:03:08 +0900

    before desighn

--- edu/test.js
+++ edu/test.js
@@ -2,64 +2,67 @@
 
 var chunks = [];
 const playbackVideo = document.getElementById('playback_video');
+const playbackVideoSource = document.getElementById('playback_video_source');
 var canv = document.querySelector('canvas');
 var stream = canv.captureStream(60);
 let blobUrl = null;
 var recorder = null;
+convertedStream = null; // deleted
 
-
+var videoBlob = null;
 var record_start_button = document.getElementById('record_start');
 var record_stop_button = document.getElementById('record_stop');
 navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
 
+
+  var img = new Image();
+  img.src = "./math.jpg?" + new Date().getTime();
+  console.log(img)
+  /* 画像が読み込まれるのを待ってから処理を続行 */
+  img.onload = function() {
+    ctx.drawImage(img, 0, 0);
+  }
 
 
 window.onload = function () {
     navigator.getUserMedia(
             {audio : true},
-            function(stream){
-                // document.querySelector('audio').src = URL.createObjectURL(stream);
-                document.querySelector('audio').srcObject = stream;
+            function(audioStream){
+                //document.querySelector('audio').srcObject = stream;
                 var audioContext = new AudioContext();
-                audioContext.createMediaStreamSource(stream);
+                audioContext.createMediaStreamSource(audioStream);
 
-
+                // todo captureStreamをstopさせてない
                 var canvasStream = canv.captureStream(60);
                 video.srcObject = canvasStream;
                 video.play();
 
-                // recorder = new MediaRecorder(canvasStream);
 
                 convertedStream = new MediaStream();
                 convertedStream.addTrack(canvasStream.getVideoTracks()[0]);
-                convertedStream.addTrack(stream.getAudioTracks()[0]);
+                convertedStream.addTrack(audioStream.getAudioTracks()[0]);
 
-
-                 var options = {
-                     audioBitsPerSecond : 128000,
-                     videoBitsPerSecond : 2500000,
-                     mimeType : 'video/webm'
-                 }
-                
-                recorder = new MediaRecorder(convertedStream);
-
-                // recorder.addEventListener('dataavailable', onRecordingReady);
+                var options = {
+                    audioBitsPerSecond : 128000,
+                    videoBitsPerSecond : 2500000,
+                    mimeType : 'video/webm'
+                }
+                recorder = new MediaRecorder(convertedStream, options);
                 recorder.ondataavailable = function(e) {
                     chunks.push(e.data);
-                    console.log(e.data.type);
                 };
 
+                recorder.onstart = function(e) {
+                    console.log("start")
+                        recorder.requestData();
+                };
                 recorder.onstop = function(e) {
                     recorder = null;
                     playRecorded();
                 };
-
-
-                recorder.addEventListener('dataavailable', onRecordingReady);
-
 
             },
             console.log
@@ -72,20 +75,35 @@ function record_save() {
 };
 
 function playRecorded() {
-    const videoBlob = new Blob(chunks, { type: "video/webm" });
+    videoBlob = new Blob(chunks, { type: "video/webm" });
     blobUrl = window.URL.createObjectURL(videoBlob);
-    console.log(blobUrl);
 
-    playbackVideo.src = blobUrl;
-    //playbackVideo.srcObject = videoBlob;
-    playbackVideo.play();
+    var videofile = new File([videoBlob], "test.webm");
+    localStorage['video'] = videofile;
+
 
     if (playbackVideo.src) {
         window.URL.revokeObjectURL(playbackVideo.src); // 解放
         playbackVideo.src = null;
     }
 
-    var link = document.createElement("a"); // Or maybe get it from the current document
+    playbackVideo.loadedmetadata = function(e) {
+        console.log("loadedmetadata");
+        console.log(e);
+    }
+
+    blobUrl = window.URL.createObjectURL(videofile);
+    playbackVideo.src = blobUrl;
+
+    playbackVideo.load();
+    playbackVideo.play();
+
+
+    console.group('metadata読み込み後');
+    console.log('長さ:', playbackVideo.duration); 
+    console.groupEnd();
+
+    var link = document.createElement("a"); 
     link.href = blobUrl;
     link.download = "video.webm";
     link.innerHTML = "Click here to download the file";
@@ -94,178 +112,24 @@ function playRecorded() {
 
 }
 
-function onRecordingReady(e) {
-    chunks.push(e.data);
-    console.log(chunks.length);
-}
-
 function record_start() {
     recorder.start(1000); 
+
+  var img = new Image();
+  img.src = "./math.jpg?" + new Date().getTime();
+  console.log(img)
+  /* 画像が読み込まれるのを待ってから処理を続行 */
+  img.onload = function() {
+    ctx.drawImage(img, 0, 0);
+  }
+
 }
 
-
-/*`
-recordButton = document.getElementById('record_start');
-stopButton = document.getElementById('record_stop');
-
-var recordButton, stopButton, recorder, liveStream;
-navigator.mediaDevices.getUserMedia({
-  audio: true,
-  //video: true
-})
-.then(function (stream) {
-  recordButton.disabled = false;
-  recordButton.addEventListener('click', startRecording);
-  stopButton.addEventListener('click', stopRecording);
-
-});
-
-
-
-function startRecording() {
-  recorder = new MediaRecorder(liveStream);
-
-  recorder.addEventListener('dataavailable', onRecordingReady);
-
-  recordButton.disabled = true;
-  stopButton.disabled = false;
-
-  recorder.start();
-}
-
-function stopRecording() {
-  recordButton.disabled = false;
-  stopButton.disabled = true;
-
-  recorder.stop();
-}
-
-function onRecordingReady(e) {
-  var video = document.getElementById('recording');
-  // e.data contains a blob representing the recording
-  video.src = URL.createObjectURL(e.data);
-  video.play();
-}
-
-*/
-
-
-
-
-/*
- *
- *
- *
- *
- *
-
-
-var recordButton, stopButton, recorder, liveStream;
-
-window.onload = function () {
-  recordButton = document.getElementById('record_start');
-  stopButton = document.getElementById('record_stop');
-
-  // get video & audio stream from user
-  navigator.mediaDevices.getUserMedia({
-    audio: true,
-    //video: true
-  })
-  .then(function (stream) {
-    recordButton.disabled = false;
-    recordButton.addEventListener('click', startRecording);
-    stopButton.addEventListener('click', stopRecording);
-
-  });
+function next_slide() {
+  var img2 = new Image();
+  img2.src = "./math2.png?" + new Date().getTime();
+  img2.onload = function() {
+    ctx.drawImage(img2, 0, 0);
+  }
 };
 
-function startRecording() {
-  recorder = new MediaRecorder(liveStream);
-
-  recorder.addEventListener('dataavailable', onRecordingReady);
-
-  recordButton.disabled = true;
-  stopButton.disabled = false;
-
-  recorder.start();
-}
-
-function stopRecording() {
-  recordButton.disabled = false;
-  stopButton.disabled = true;
-
-  recorder.stop();
-}
-
-function onRecordingReady(e) {
-  var video = document.getElementById('recording');
-  // e.data contains a blob representing the recording
-  video.src = URL.createObjectURL(e.data);
-  video.play();
-}
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var recordButton, stopButton, recorder, liveStream;
-
-window.onload = function () {
-  recordButton = document.getElementById('record');
-  stopButton = document.getElementById('stop');
-
-  // get video & audio stream from user
-  navigator.mediaDevices.getUserMedia({
-    audio: true
-  })
-  .then(function (stream) {
-    var liveVideo = document.getElementById('live');
-    liveVideo.src = URL.createObjectURL(stream);
-    liveVideo.play();
-
-    recordButton.disabled = false;
-    recordButton.addEventListener('click', startRecording);
-    stopButton.addEventListener('click', stopRecording);
-
-  });
-};
-
-function startRecording() {
-  recorder = new MediaRecorder(liveStream);
-
-  recorder.addEventListener('dataavailable', onRecordingReady);
-
-  recordButton.disabled = true;
-  stopButton.disabled = false;
-
-  recorder.start();
-}
-
-function stopRecording() {
-  recordButton.disabled = false;
-  stopButton.disabled = true;
-
-  // Stopping the recorder will eventually trigger the 'dataavailable' event and we can complete the recording process
-  recorder.stop();
-}
-
-function onRecordingReady(e) {
-  var video = document.getElementById('recording');
-  // e.data contains a blob representing the recording
-  video.src = URL.createObjectURL(e.data);
-  video.play();
-}
-*/
